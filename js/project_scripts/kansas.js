ceed012e77090177ef832bd3a7a9de5573edfc65
radio412 <jbergexperiencedesign@gmail.com>
2017-12-07 15:58:33 -0600

    added url for example

--- js/project_scripts/kansas.js
+++ js/project_scripts/kansas.js
@@ -1,6 +1,6 @@
 
 function init(){
-  var toto = new Dorothy("pk.eyJ1IjoiamVmZmIiLCJhIjoiY2oxeHExYnp2MDAwaTJxcXRmcjYxamc4aSJ9.CarCFkK2uSkUA2bHsJMTvQ");
+  var toto = new Dorothy(" YOUR MAP BOX KEY ");
   toto.camStarts = [new THREE.Vector3(30,30,30),new THREE.Vector3(0,100,0),new THREE.Vector3(100,50,0),new THREE.Vector3(0,20,1400)];
   toto.clickHeels("default",  document.getElementById("threetl"),document.getElementById("threetr"), document.getElementById("threebl") ,document.getElementById("threebr"));
   toto.showGrid(500,100);
